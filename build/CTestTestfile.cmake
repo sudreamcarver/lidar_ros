6887dbbe258c42371d102672cbab5ac03a54017a
carver <lt72496@outlook.com>
2025-11-17 16:20:11 +0800

    0.1

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/lidar_ws/src
-# Build directory: /root/lidar_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lidar_pkg")
